0d056102e9a4f450c66fbbf96172d63c427077e0
purplesmile3 <puritychepkurui5@gmail.com>
2020-05-11 17:28:09 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-const app = async () => '#BuildforSDG';
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors= require('cors');
+
+const postRoute= require('./routes/postRoute');
+
+const app= express();
+app.use(cors);
+app.use(bodyParser.json());
+
+
+//connecting to mongoose cluster
+mongoose.connect('mongodb+srv://purity:RrWs8bKSsOhTgv9p@backend-i5epy.mongodb.net/test?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+  app.use('/api/posts',postRoute);
+
 
 export default app;
